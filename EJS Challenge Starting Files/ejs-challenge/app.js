c66f21f717cfe8bba7bc1ec46049376ac0cf79f7
Your Name <your_email@example.com>
2023-06-01 16:15:09 +0300

    v9

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisistie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictuuisque sagitt cursus turpis massa tincidunt dui.";
@@ -13,15 +14,13 @@ let items = [];
 
 app.set('view engine', 'ejs');
 
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
 app.get("/" , function (req, res) {
-  res.render("home", {StartingContent: homeStartingContent , posİte: items });
+  res.render("home", {StartingContent: homeStartingContent, posİte: items});
 });
+
 app.get("/contact" , function (req, res) {
   res.render("contact", {contactC: contactContent});
 });
@@ -30,40 +29,31 @@ app.get("/about" , function (req, res) {
   res.render("about", {aboutC: aboutContent});
 });
 
-app.get("/posts/:postName" , function (req, res) {
+app.get("/items/:postName" , function (req, res) {
   const requestedPost = req.params.postName;
 
-  posts.forEach(function (post) {
-    
-    if (post.title === requestedPost) {
-      res.render("post", {post: post});
+  items.forEach(function (post) {
+    const stTitle =post.title
+    if (stTitle === requestedPost) {
+      
+      console.log("eşleşti")
     }
   });
 });
 
-
-
 app.get("/compose" , function (req, res) {
   res.render("compose");
 });
+
 app.post('/compose', (req, res) => {
-  var post = {
-    title :  req.body.mdeger,
-    body  : req.body.zdeger
- };
-items.push(post);
-console.log(post);
-res.redirect('/')
+  const post = {
+    title: req.body.mdeger,
+    body: req.body.zdeger
+  };
+  items.push(post);
+  console.log(post);
+  res.redirect('/');
 });
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
